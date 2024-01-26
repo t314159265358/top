c327d9600cc232021d1e1996f00ca30a665cae62
Cloud <cloudinstone@gmail.com>
2024-01-27 00:05:13 +0800

    dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,9 @@ require('laravel-mix-obfuscator');
 
 mix.js('src/scripts/app.js', 'dist/scripts')
     .sass('src/styles/app.scss', 'dist/styles')
-    .sourceMaps(false)
-    .obfuscator();
+
+if (mix.inProduction()) {
+    mix.obfuscator();
+} else {
+    mix.sourceMaps(false);
+}
